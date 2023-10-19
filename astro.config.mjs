6bccdc94d7e5c757a2d51489fdbc32cd81d5ec15
Chris Kolenko <chris@webcanvas.com.au>
2023-10-19 13:30:28 +1100

    Add analytics and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import partytown from '@astrojs/partytown';
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
-  integrations: [react(), tailwind()],
+  site: 'https://www.getnoops.com',
+  integrations: [
+    react(), 
+    tailwind(),
+    sitemap(),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+  ],
 });
