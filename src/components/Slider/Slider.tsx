bf2f4547df79614f1154a52e96baceded0239cc5
calvinmoylan <calvinmoylan1@gmail.com>
2023-12-05 15:18:31 +0800

    slider fix

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -10,15 +10,19 @@ const Slider = React.forwardRef<
 >(({ noopsColor, className, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
-    className={classNames("relative flex w-full  items-center", className)}
+    className={classNames(
+      "relative flex w-full touch-none select-none items-center",
+      className,
+    )}
     {...props}
   >
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-noops-200">
       <SliderPrimitive.Range className="absolute h-full bg-gradient-to-r from-noops-600 to-accent" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="ring-offset-background  block h-5 w-5 rounded-full border-2 border-accent bg-gradient-to-t from-noops-600 to-accent shadow-inner shadow-white/20 ring-accent transition-colors focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="ring-offset-background block h-5  w-5 touch-auto select-auto rounded-full border-2 border-accent bg-gradient-to-t from-noops-600 to-accent shadow-inner shadow-white/20 ring-accent transition-colors focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
+
 Slider.displayName = SliderPrimitive.Root.displayName;
 
 export default Slider;
